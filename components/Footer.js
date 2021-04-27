cb61cb72726ef37b4d67fa1aa85e5032f0ab71c6
Jools Barnett <vitamins999@gmail.com>
2021-04-27 11:45:42 +0100

    ui: reduced padding on header and footer to look more aesthetically pleasing

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer className='fixed flex w-full justify-end md:bottom-10 bottom-5 z-10 xl:px-56 md:px-28 px-10 hover:text-green-700 transition ease-in-out duration-150'>
+    <footer className='fixed flex w-full justify-end md:bottom-10 bottom-5 z-10 px-16 py-4 hover:text-green-700 transition ease-in-out duration-150'>
       <Link href='#'>
         <a>
           <Image src='/icons/github-logo-32.png' width={32} height={32} />
