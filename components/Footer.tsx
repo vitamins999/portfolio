9cfd0368b89518225fd024515f524bb1ffc5e6c7
Jools <vitamins999@gmail.com>
2021-05-04 16:10:31 +0100

    ui: added hover animation to github icon (#19)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 const Footer = () => {
   return (
     <footer className='fixed flex w-full justify-end md:bottom-10 bottom-5 z-10 px-16 py-4 hover:text-green-700 transition ease-in-out duration-150'>
       <Link href='#'>
-        <a>
+        <motion.a whileHover={{ translateY: -7 }} className='cursor-pointer'>
           <Image src='/icons/github-logo-32.png' width={32} height={32} />
-        </a>
+        </motion.a>
       </Link>
     </footer>
   );
