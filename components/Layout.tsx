383b4838f3199c6b37affafe5cb59c99e57ad3e5
Jools <vitamins999@gmail.com>
2021-05-10 13:32:47 +0100

    ui: added favicon (#24)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,6 +17,18 @@ const Layout = ({ children, title }: Props) => {
     <>
       <Head>
         <title>Jools Barnett | {title}</title>
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/icons/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/icons/favicon-16x16.png'
+        />
       </Head>
       <div className='font-bodyMain'>
         <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
