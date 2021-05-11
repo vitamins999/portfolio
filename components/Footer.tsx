470c0bc8ceaa18d7c080d0a9c4fb657d53f9919b
Jools <vitamins999@gmail.com>
2021-05-11 13:11:03 +0100

    ui: hide github link when menu open (#26)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,17 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 
-const Footer = () => {
+type Props = {
+  menuOpen: boolean;
+};
+
+const Footer = ({ menuOpen }: Props) => {
   return (
-    <footer className='fixed flex w-full justify-end md:bottom-10 bottom-5 z-10 px-16 py-4 hover:text-green-700 transition ease-in-out duration-150'>
+    <footer
+      className={`${
+        menuOpen ? 'hidden' : 'null'
+      } fixed flex w-full justify-end md:bottom-10 bottom-5 z-10 px-16 py-4 hover:text-green-700 transition ease-in-out duration-150`}
+    >
       <Link href='#'>
         <motion.a whileHover={{ translateY: -7 }} className='cursor-pointer'>
           <Image src='/icons/github-logo-32.png' width={32} height={32} />
