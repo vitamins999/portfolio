616a70fae3de451a6d91ff68fb0af71b7282d094
Jools Barnett <vitamins999@gmail.com>
2021-06-29 11:19:00 +0100

    added indepth details for Watchlist and edited placeholder text on main page

--- pages/projects/watchlist.tsx
+++ pages/projects/watchlist.tsx
@@ -9,6 +9,7 @@ const WatchlistPage = () => {
   return (
     <Layout title={title}>
       <motion.main
+        className='pb-10'
         variants={fadeOutPage}
         exit='exit'
         initial='initial'
@@ -28,12 +29,20 @@ const WatchlistPage = () => {
                 </h2>
               </div>
               <div className='flex lg:flex-row flex-col items-center'>
-                <a href='#' target='_blank' rel='noreferrer'>
+                <a
+                  href='https://watchlistmovies.vercel.app'
+                  target='_blank'
+                  rel='noreferrer'
+                >
                   <button className='py-4 px-14 lg:my-0 my-5 lg:mr-5 uppercase font-bold text-xs tracking-wider bg-gray-50 text-gray-800 transition duration-150 hover:bg-gray-800 hover:text-gray-50 font-bodyEandE focus:outline-none'>
                     Visit Site
                   </button>
                 </a>
-                <a href='#' target='_blank' rel='noreferrer'>
+                <a
+                  href='https://github.com/vitamins999/watchlist-2.0'
+                  target='_blank'
+                  rel='noreferrer'
+                >
                   <button className='py-4 px-14 uppercase font-bold text-xs tracking-wider bg-gray-50 text-gray-800 transition duration-150 hover:bg-gray-800 hover:text-gray-50 font-bodyEandE focus:outline-none'>
                     View Code on Github
                   </button>
@@ -50,7 +59,8 @@ const WatchlistPage = () => {
                 Frontend
               </h2>
               <p className='font-bodyMain text-gray-700'>
-                NextJS, TailwindCSS, Framer Motion
+                Next.js, TailwindCSS, Framer Motion, React Hook Form, Context
+                API
               </p>
             </div>
             <div className='py-3'>
@@ -58,109 +68,162 @@ const WatchlistPage = () => {
                 Backend
               </h2>
               <p className='font-bodyMain text-gray-700'>
-                NextJS Serverless, Axios, Cheerio
+                Next.js Serverless, Axios, Cheerio
               </p>
             </div>
           </div>
         </section>
         <section className='lg:px-84 iPadPro:px-32 iPadWidescreen:px-32 px-10 lg:pb-10 pt-5 pb-5'>
           <div>
-            <h3 className='text-3xl font-headingMain py-3'>Problem</h3>
+            <h3 className='text-3xl font-headingMain py-3'>
+              Why Does This Exist?
+            </h3>
+            <div className='border-t w-full pb-7'></div>
             <p className='pt-5'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ex
-              quisquam veniam maxime facilis sunt voluptatibus nemo nobis
-              molestiae. Facilis nobis molestias illum quo. Quod quas quae quis
-              in consectetur officia, tenetur laborum doloribus voluptates ipsam
-              praesentium et assumenda nobis nihil aut asperiores placeat!
-              Consequatur rerum cum, inventore deserunt repudiandae blanditiis,
-              iusto reprehenderit accusantium qui, enim animi temporibus
-              delectus saepe!
+              There are two main sites on the web that have the ability to
+              curate lists of films:{' '}
+              <a
+                className='underline text-blue-700'
+                href='https://letterboxd/'
+                rel='noreferrer noopener'
+                target='_blank'
+              >
+                Letterboxd
+              </a>{' '}
+              and{' '}
+              <a
+                className='underline text-blue-700'
+                href='https://icheckmovies.com/'
+                rel='noreferrer noopener'
+                target='_blank'
+              >
+                icheckmovies.com
+              </a>
+              .
             </p>
             <p className='pt-5'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
-              quisquam voluptatem, rerum dolorum aut quas natus qui quos
-              molestias facilis quo. Iusto exercitationem natus nemo, adipisci
-              veritatis maiores officia totam accusantium odit repudiandae velit
-              vero voluptatem, sunt veniam commodi quasi.
+              Now, whilst Letterboxd already has the ability to see what films
+              on a curated list are available to stream, the site itself is more
+              intended as a social media platform, rather than having lists of
+              films as the website's main purpose. As such, their list function
+              is a little disorganised and very much centered on user curated
+              lists.
             </p>
             <p className='pt-5'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quod
-              provident laborum nesciunt porro blanditiis qui facere! Totam id,
-              delectus laborum voluptas rem hic minus quod cumque, voluptate
-              reiciendis deserunt natus perspiciatis laboriosam animi voluptatem
-              quo harum esse maiores nam officiis? Eum dolorum quasi rerum illo
-              error voluptas aspernatur modi maxime sit consequatur vel nobis
-              quis deserunt, dolor sunt nostrum?
-            </p>
-          </div>
-          <div className='pt-10'>
-            <h3 className='text-3xl font-headingMain py-3'>Solution</h3>
-            <p className='pt-5'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Delectus, perferendis amet vel nam, exercitationem accusamus
-              consequatur minima necessitatibus totam, atque nesciunt. Ipsa
-              reprehenderit deleniti inventore alias! Ducimus reiciendis quidem
-              cum placeat! Culpa vero nihil odio minima atque corrupti esse
-              perferendis, totam, accusamus voluptas voluptatibus. Saepe
-              accusamus recusandae, veniam officia libero voluptates expedita
-              asperiores ex rem nihil in culpa ut inventore.
+              icheckmovies, on the other hand, has a far more organised
+              selection of lists, many of them officially curated by the admin.
+              However, they do not have the ability to see what films on those
+              lists are available for streaming.
             </p>
             <p className='pt-5'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
-              officia doloribus totam sit! Consequatur ad ut nostrum nisi neque
-              minima eum mollitia sunt nobis reiciendis quod dolor iusto eos
-              rerum explicabo ducimus a doloremque ullam ipsum corrupti, non
-              odio vel nulla? Recusandae eaque, deserunt dicta officia ex enim.
-              Aperiam necessitatibus tenetur et corrupti, fugiat voluptas nisi
-              sed in maxime quibusdam, aspernatur nesciunt! Est, suscipit dolor
-              accusamus adipisci quasi eaque magni porro saepe dicta maiores sit
-              aut corporis placeat temporibus quam!
+              There are many people out there (including myself) who prefer to
+              use icheckmovies when deciding on what film they're in the mood to
+              watch, based on what films in a given list they haven't seen yet.
             </p>
             <p className='pt-5'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-              vero fugiat praesentium aspernatur, totam mollitia maiores earum
-              placeat eius dolores magnam necessitatibus, officiis temporibus
-              suscipit eaque. Quam, id temporibus! Molestiae?
+              <span className='italic'>Watchlist</span> was created to solve
+              this problem.
             </p>
           </div>
           <div className='pt-10'>
             <h3 className='text-3xl font-headingMain py-3'>
-              Key Design Choices
+              How Does It Work?
             </h3>
-            <ul className='list-disc lg:list-inside lg:px-0 px-5'>
-              <li className='pt-5'>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
-                quas voluptatum numquam labore doloribus rerum, cumque sunt
-                blanditiis cum excepturi?
+            <div className='border-t w-full pb-7'></div>
+            <p className='pt-5'>
+              Scraping and API calls to the{' '}
+              <a
+                className='underline text-blue-700'
+                href='https://themoviedb.org/'
+                rel='noreferrer noopener'
+                target='_blank'
+              >
+                The Movie Database
+              </a>
+              .
+            </p>
+            <p className='pt-5 pb-1'>To put it simply:</p>
+            <ol className='list-decimal list-inside'>
+              <li className='pt-2'>
+                The user enters the URL of an{' '}
+                <a
+                  className='underline text-blue-700'
+                  href='https://icheckmovies.com/'
+                  rel='noreferrer noopener'
+                  target='_blank'
+                >
+                  icheckmovies.com
+                </a>{' '}
+                list (i.e.{' '}
+                <a
+                  className='underline text-blue-700'
+                  href='https://icheckmovies.com/lists/imdbs+top+250/'
+                  rel='noreferrer noopener'
+                  target='_blank'
+                >
+                  https://icheckmovies.com/lists/imdbs+top+250/
+                </a>
+                ) and a region.
               </li>
-              <li className='pt-5'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Obcaecati illum, architecto ipsum tempora debitis aliquam!
-                Provident quos eligendi officiis perspiciatis fugiat, aliquam
-                atque obcaecati delectus eius nihil dolorum a et quibusdam eum
-                saepe assumenda qui natus quam facere minima aliquid!
+              <li className='pt-2'>
+                Axios scrapes the url's page. This data is then used to create
+                an object containing the list title and an array of the listed
+                films and their release year.
               </li>
-              <li className='pt-5'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Veritatis, possimus vel molestiae cum perferendis facere dolore!
-                Nobis error possimus eos veniam voluptate quis enim provident
-                accusantium aliquam. Voluptates, quae est.
+              <li className='pt-2'>
+                The app loops through this array, making two API calls to The
+                Movie Database for each film. The first call gets the relevant
+                metadata for the film, including the ID for the film on The
+                Movie Database. The second call uses this ID along with the
+                region, to find out what streaming providers in that region are
+                currently streaming the film. If it's either Netflix or Amazon
+                Prime, this information along with the relevant metadata is
+                pushed to an All Films array.
               </li>
-              <li className='pt-5'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptas porro inventore quisquam mollitia unde, quam aliquam at
-                quia temporibus tenetur soluta earum nam dolor consequuntur
-                obcaecati eos ipsa voluptatum quo. Ab vero similique, soluta sit
-                quasi reiciendis facere dolor tenetur, non rem aliquam qui
-                excepturi? Necessitatibus odit officia eligendi dignissimos!
+              <li className='pt-2'>
+                Once the loop has finished, the content of the All Films array
+                is displayed to the user.
               </li>
-              <li className='pt-5'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-                maxime? Possimus officiis ipsum expedita earum unde ipsa!
-                Facilis quisquam ipsam officiis debitis veniam excepturi sunt
-                repellat ad animi! Molestiae, velit!
-              </li>
-            </ul>
+            </ol>
+          </div>
+          <div className='pt-10'>
+            <h3 className='text-3xl font-headingMain py-3'>Hosting</h3>
+            <div className='border-t w-full pb-7'></div>
+            <p className='pt-5'>
+              <span className='italic'>Watchlist</span> is hosted on{' '}
+              <a
+                className='underline text-blue-700'
+                href='https://vercel.com'
+                rel='noreferrer noopener'
+                target='_blank'
+              >
+                Vercel.com
+              </a>{' '}
+              in order to take advantage of Next.js's serverless functions.
+            </p>
+            <p className='pt-5'>
+              This allows the API calls that scrape{' '}
+              <a
+                className='underline text-blue-700'
+                href='https://icheckmovies.com/'
+                rel='noreferrer noopener'
+                target='_blank'
+              >
+                icheckmovies.com
+              </a>{' '}
+              and get the relevant data from{' '}
+              <a
+                className='underline text-blue-700'
+                href='https://themoviedb.org/'
+                rel='noreferrer noopener'
+                target='_blank'
+              >
+                The Movie Database
+              </a>{' '}
+              to be abstracted away from the client, without having to build a
+              specific backend (in Express, for example) for what one could
+              consider some relatively simple backend code.
+            </p>
           </div>
         </section>
       </motion.main>
