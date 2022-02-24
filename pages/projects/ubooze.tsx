4713926191a7badf8c908611521d52afb5ab86f0
Jools Barnett <vitamins999@gmail.com>
2022-02-24 20:27:38 +0000

    Rewrote Ubooze project page to be far more in depth

--- pages/projects/ubooze.tsx
+++ pages/projects/ubooze.tsx
@@ -59,8 +59,8 @@ const UboozePage = () => {
                 Frontend
               </h2>
               <p className='font-bodyMain text-gray-700'>
-                NextJS, Redux, React Hook Form, React Query, TailwindCSS, Framer
-                Motion
+                Next.js, Redux, React Hook Form, React Query, Tailwind CSS,
+                Framer Motion
               </p>
             </div>
             <div className='py-3'>
@@ -68,118 +68,296 @@ const UboozePage = () => {
                 Backend
               </h2>
               <p className='font-bodyMain text-gray-700'>
-                Express, PassportJS, bcrypt, JSON Web Tokens, OAuth, Axios,
-                Puppeteer, Cheerio, SendGrid
+                Node.js, Express.js, Passport.js, bcrypt, JSON Web Tokens,
+                OAuth, Axios, Puppeteer, Cheerio, SendGrid, Knex.js,
+                Objection.js
               </p>
             </div>
             <div className='py-3'>
               <h2 className='font-bodyMain text-xl font-semibold tracking-wide'>
                 Database
               </h2>
-              <p className='font-bodyMain text-gray-700'>
-                PostgreSQL, KnexJS, ObjectionJS
-              </p>
+              <p className='font-bodyMain text-gray-700'>PostgreSQL</p>
             </div>
           </div>
         </section>
         <section className='lg:px-84 iPadPro:px-32 iPadWidescreen:px-32 px-10 lg:pb-10 pt-5 pb-5'>
           <div>
-            <h3 className='text-3xl font-headingMain py-3'>Problem</h3>
             <p className='pt-5'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ex
-              quisquam veniam maxime facilis sunt voluptatibus nemo nobis
-              molestiae. Facilis nobis molestias illum quo. Quod quas quae quis
-              in consectetur officia, tenetur laborum doloribus voluptates ipsam
-              praesentium et assumenda nobis nihil aut asperiores placeat!
-              Consequatur rerum cum, inventore deserunt repudiandae blanditiis,
-              iusto reprehenderit accusantium qui, enim animi temporibus
-              delectus saepe!
+              Alcohol is expensive. That's why I created{' '}
+              <span className='italic'>Ubooze</span> -- a price comparison
+              website for alcohol prices at your local UK supermarkets. Simply
+              type in your postcode and see what drinks are cheapest near you.{' '}
             </p>
             <p className='pt-5'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
-              quisquam voluptatem, rerum dolorum aut quas natus qui quos
-              molestias facilis quo. Iusto exercitationem natus nemo, adipisci
-              veritatis maiores officia totam accusantium odit repudiandae velit
-              vero voluptatem, sunt veniam commodi quasi.
+              Don't care about finding local prices? No problem.{' '}
+              <span className='italic'>Ubooze</span> also includes the ability
+              to let the user select which supermarkets they want to compare
+              against each other. No postcode necessary.{' '}
             </p>
             <p className='pt-5'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quod
-              provident laborum nesciunt porro blanditiis qui facere! Totam id,
-              delectus laborum voluptas rem hic minus quod cumque, voluptate
-              reiciendis deserunt natus perspiciatis laboriosam animi voluptatem
-              quo harum esse maiores nam officiis? Eum dolorum quasi rerum illo
-              error voluptas aspernatur modi maxime sit consequatur vel nobis
-              quis deserunt, dolor sunt nostrum?
+              And that's not all. The <span className='italic'>Ubooze</span>{' '}
+              website also includes full login/register functionality for user
+              accounts (as well as Facebook and Google sign ins through OAuth),
+              allowing users to favourite products and create their own public
+              favourite alcohol pages, for easy access to their favourite
+              product price comparisons in the future.
             </p>
           </div>
           <div className='pt-10'>
-            <h3 className='text-3xl font-headingMain py-3'>Solution</h3>
+            <h3 className='text-3xl font-headingMain py-3'>Repositories</h3>
+            <div className='border-t w-full pb-7'></div>
             <p className='pt-5'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Delectus, perferendis amet vel nam, exercitationem accusamus
-              consequatur minima necessitatibus totam, atque nesciunt. Ipsa
-              reprehenderit deleniti inventore alias! Ducimus reiciendis quidem
-              cum placeat! Culpa vero nihil odio minima atque corrupti esse
-              perferendis, totam, accusamus voluptas voluptatibus. Saepe
-              accusamus recusandae, veniam officia libero voluptates expedita
-              asperiores ex rem nihil in culpa ut inventore.
+              <span className='italic'>Ubooze</span> functions through the use
+              of two repositories:
             </p>
             <p className='pt-5'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
-              officia doloribus totam sit! Consequatur ad ut nostrum nisi neque
-              minima eum mollitia sunt nobis reiciendis quod dolor iusto eos
-              rerum explicabo ducimus a doloremque ullam ipsum corrupti, non
-              odio vel nulla? Recusandae eaque, deserunt dicta officia ex enim.
-              Aperiam necessitatibus tenetur et corrupti, fugiat voluptas nisi
-              sed in maxime quibusdam, aspernatur nesciunt! Est, suscipit dolor
-              accusamus adipisci quasi eaque magni porro saepe dicta maiores sit
-              aut corporis placeat temporibus quam!
+              1. The Web App (
+              <a
+                href='https://github.com/vitamins999/uBooze'
+                target='_blank'
+                rel='noreferrer'
+                className='underline text-blue-700'
+              >
+                found here
+              </a>
+              )
+            </p>
+            <p>
+              {' '}
+              2. The Supermarket Webscraper Utility (
+              <a
+                href='https://github.com/vitamins999/ubooze-scraper'
+                target='_blank'
+                rel='noreferrer'
+                className='underline text-blue-700'
+              >
+                found here
+              </a>
+              )
             </p>
             <p className='pt-5'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-              vero fugiat praesentium aspernatur, totam mollitia maiores earum
-              placeat eius dolores magnam necessitatibus, officiis temporibus
-              suscipit eaque. Quam, id temporibus! Molestiae?
+              The <span className='italic'>Ubooze</span> Supermarket Scraper
+              Utility is a custom made Node.js web scraper for UK supermarket
+              alcohol prices, with PostgreSQL database integration for insertion
+              and updating of those prices.
+            </p>
+            <p className='pt-5'>
+              The scraper is intended for use with the{' '}
+              <span className='italic'>Ubooze</span> web app (this repository),
+              in order keep the website's database up to date, with the most
+              recent alcohol prices and offers.
+            </p>
+            <p className='pt-5'>It uses Node.js to run in the command line.</p>
+            <p className='pt-5'>
+              For more information, please refer to the{' '}
+              <a
+                href='https://github.com/vitamins999/ubooze-scraper'
+                target='_blank'
+                rel='noreferrer'
+                className='underline text-blue-700'
+              >
+                Ubooze Web Scraper Repository
+              </a>
+              .
             </p>
           </div>
           <div className='pt-10'>
             <h3 className='text-3xl font-headingMain py-3'>
-              Key Design Choices
+              Why Does This Exist?
             </h3>
-            <ul className='list-disc lg:list-inside lg:px-0 px-5'>
-              <li className='pt-5'>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
-                quas voluptatum numquam labore doloribus rerum, cumque sunt
-                blanditiis cum excepturi?
-              </li>
-              <li className='pt-5'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Obcaecati illum, architecto ipsum tempora debitis aliquam!
-                Provident quos eligendi officiis perspiciatis fugiat, aliquam
-                atque obcaecati delectus eius nihil dolorum a et quibusdam eum
-                saepe assumenda qui natus quam facere minima aliquid!
-              </li>
-              <li className='pt-5'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Veritatis, possimus vel molestiae cum perferendis facere dolore!
-                Nobis error possimus eos veniam voluptate quis enim provident
-                accusantium aliquam. Voluptates, quae est.
-              </li>
-              <li className='pt-5'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptas porro inventore quisquam mollitia unde, quam aliquam at
-                quia temporibus tenetur soluta earum nam dolor consequuntur
-                obcaecati eos ipsa voluptatum quo. Ab vero similique, soluta sit
-                quasi reiciendis facere dolor tenetur, non rem aliquam qui
-                excepturi? Necessitatibus odit officia eligendi dignissimos!
-              </li>
-              <li className='pt-5'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-                maxime? Possimus officiis ipsum expedita earum unde ipsa!
-                Facilis quisquam ipsam officiis debitis veniam excepturi sunt
-                repellat ad animi! Molestiae, velit!
-              </li>
-            </ul>
+            <div className='border-t w-full pb-7'></div>
+            <p className='pt-5'>
+              As I already mentioned above, alcohol is expensive. It is also
+              something that routinely changes price quite dramatically from
+              shop to shop, further complicated by any special offers offered by
+              these shops.
+            </p>
+            <p className='pt-5'>
+              Due to this high level of expense, it also means that saving money
+              on alcohol will have a greater effect on an overall shopping bill
+              than saving money on most other food related products. Something
+              of keen interest to financial trying times, like... well, right
+              now.
+            </p>
+            <p className='pt-5'>
+              So wouldn't it be nice if there was a website to compare alcohol
+              prices across supermarkets at a glance?
+            </p>
+            <p className='pt-5'>
+              Enter <span className='italic'>Ubooze</span>.
+            </p>
+            <p className='pt-5'>
+              I conducted extensive market research, and discovered that not
+              only was there both a need and a gap in the market for this
+              product, but prospective users also valued supermarkets that were
+              closer to them over supermarkets further away.
+            </p>
+            <p className='pt-5'>
+              This lead to the final piece of the puzzle -- a price comparison
+              website for supermarket alcohol prices where the user could
+              either:
+            </p>
+            <p className='pt-5'>1. Select which supermarkets to compare; or</p>
+            <p>
+              {' '}
+              2. Enter a radius and a postcode, and compare supermarkets only
+              found within that radius.
+            </p>
+          </div>
+          <div className='pt-10'>
+            <h3 className='text-3xl font-headingMain py-3'>
+              How Does It Work?
+            </h3>
+            <div className='border-t w-full pb-7'></div>
+            <p className='pt-5'>
+              <span className='italic'>Ubooze</span> is made up of two distinct
+              parts:
+            </p>
+            <p className='pt-5'>
+              1. A full stack Web App with REST API (
+              <a
+                href='https://github.com/vitamins999/uBooze'
+                target='_blank'
+                rel='noreferrer'
+                className='underline text-blue-700'
+              >
+                found here
+              </a>
+              )
+            </p>
+            <p>
+              2. A custom made Web Scraper Utility (
+              <a
+                href='https://github.com/vitamins999/ubooze-scraper'
+                target='_blank'
+                rel='noreferrer'
+                className='underline text-blue-700'
+              >
+                found here
+              </a>
+              )
+            </p>
+            <p className='pt-5'>
+              The web scraper utility is a Node.js program that runs in the
+              command line, which the website admin runs in order to populate
+              the database, whenever they feel it appropriate. Its purpose is to
+              scrape current alcohol prices and offers from the most popular UK
+              supermarkets' websites.
+            </p>
+            <p className='pt-5'>
+              After a successful scrape, it connects to a PostgreSQL database
+              hosted in the cloud (I decided to use{' '}
+              <a
+                className='underline text-blue-700'
+                href='https://supabase.com/'
+                rel='noreferrer noopener'
+                target='_blank'
+              >
+                Supabase
+              </a>
+              , due to the very generous free tier allowing me to minimise
+              hosting costs) and updates already existing products to their new
+              price and/or offer, or inserts new products into the database, to
+              be manually linked up and made visible outside the database, in
+              the future.
+            </p>
+            <p className='pt-5'>
+              The Web App is a full stack web application, utilising Next.js on
+              the client and Express.js on the server.
+            </p>
+            <p className='pt-5'>
+              When the user visits the website, they have the option of either
+              selecting price comparison by clicking the supermarkets to
+              compare, or having the website choose which supermarkets to
+              compare by entering the user's postcode and a radius of 1-3 miles.
+            </p>
+            <p className='pt-5'>
+              If the user selects the supermarkets to compare, a query is made
+              to the database to select all products where the 'supermarket'
+              field corresponds to those supermarkets.
+            </p>
+            <p className='pt-5'>
+              If the user enters their postcode and a radius, a request
+              containing the postcode is sent by the REST API on the server to
+              the Mapbox API. This returns the latitude and longitude of the
+              postcode. These coordinates are then sent as a request, along with
+              the radius converted to kilometres, to the Foursquare API. This
+              returns a JSON back to the server, of the supermarkets found
+              within the radius of those coordinates.
+            </p>
+            <p className='pt-5'>
+              The website then queries the database, as above, with this list of
+              supermarkets.
+            </p>
+            <p className='pt-5'>
+              In both case, the client receives a paginated list of items from
+              the database, that correspond to the relevant supermarket(s).
+            </p>
+            <p className='pt-5'>
+              The website also features the ability to search for a specific
+              drink. This will query the database and return matching products
+              from all supermarkets and their comparative prices.
+            </p>
+            <p className='pt-5'>
+              Furthermore, the website also allows users to register accounts
+              and login (along with OAuth integration for Google and Facebook).
+              From here, they can choose to favourite a product, whereby
+              favourited productIDs are saved in the database for that user. If
+              they return to their favourite page in the future, a request is
+              made to the database to return products that are found in their
+              favourite product field, thus populating their product page with
+              all of their favourite products.
+            </p>
+            <p className='pt-5'>
+              Essentially, <span className='italic'>Ubooze</span> is a standard
+              CRUD full stack web app, where the database's data (with the
+              exception of user data) is populated by a Node.js command line web
+              scraper.
+            </p>
+          </div>
+          <div className='pt-10'>
+            <h3 className='text-3xl font-headingMain py-3'>Hosting</h3>
+            <div className='border-t w-full pb-7'></div>
+            <p className='pt-5'>
+              The frontend is hosted on{' '}
+              <a
+                className='underline text-blue-700'
+                href='https://vercel.com'
+                rel='noreferrer noopener'
+                target='_blank'
+              >
+                Vercel.com
+              </a>{' '}
+              in order to take advantage of their free Next.js hosting and image
+              optimisation.
+            </p>
+            <p className='pt-5'>
+              The backend is hosted on{' '}
+              <a
+                className='underline text-blue-700'
+                href='https://heroku.com'
+                rel='noreferrer noopener'
+                target='_blank'
+              >
+                Heroku.com
+              </a>
+              's hobby tier. This is because it allows for 100% uptime, whereas
+              the free tier will power down after 30 minutes.
+            </p>
+            <p className='pt-5'>
+              The PostgreSQL database is hosted on{' '}
+              <a
+                className='underline text-blue-700'
+                href='https://supabase.com/'
+                rel='noreferrer noopener'
+                target='_blank'
+              >
+                Supabase
+              </a>
+              , due to their very generous free tier, which is more than enough
+              for the needs of the site.
+            </p>
           </div>
         </section>
       </motion.main>
